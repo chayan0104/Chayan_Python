4edd9832ed526b9abe60c5937227a879e0896f8b
chayan0104 <chayansamanta8@gmail.com>
2025-02-10 00:46:46 +0530

    new

--- demolist_manupulations2.py
+++ demolist_manupulations2.py
@@ -3,27 +3,27 @@ employee_names = ["Charlie", "David", "Eve", "Alice", "Bob"]
 # Original list
 print("Original List:", ",".join(employee_names))
 
-# Inserts an element at the specified index
+# Inserts at specified index
 employee_names.insert(1,"Ram")
 print("After insert:", ",".join(employee_names))
 
-# Removes and returns the element at the specified index (default is the last item)
+#Removes and returns a avlue in particular position
 popped_item = employee_names.pop(1)
 print("After pop (index 3):", ",".join(employee_names), "Popped:", popped_item)
 
-# #Removes the first occurrence of the specified value
+#Removes bt value
 employee_names.remove("Bob")
 print("After remove (Bob):", ",".join(employee_names))
 
-# #Deletes the item at a specific index or the entire list
+#Deleting by Index
 del employee_names[1]
 print("After del (index 1):", ",".join(employee_names))
 
-# #Change an item in the list** - Modify an element at a specific index
+#Replacing a vaue in particular postion
 employee_names[2]="zara"
 print("After changing index 2 to Zara:", ",".join(employee_names))
 
-# #This works on strings, but I'll show an example here.
+#Spliting sentence
 sentence = "Alice, Bob, Charlie"
 split_names = sentence.split(",")
 print("After split:", split_names)
