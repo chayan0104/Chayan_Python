8f58043bf6393f480f6e6c860bd11ba6dfc6f356
chayan0104 <chayansamanta8@gmail.com>
2025-02-19 01:56:13 +0530

    excersise

---
+++ 04_data_structures/4_Sets/excersise.py
@@ -0,0 +1,17 @@
+
+
+list1 =[1,2,3,4,5,6]
+list2 =[4,5,6,7,8]
+
+
+# Missing values in list1
+missing_in_list1=set(list2).difference(set(list1))
+print("Missing values in list1:",missing_in_list1)
+
+# Additional vales in list1
+additional_in_list1 = set(list1).difference(set(list2))
+print("Additional values in list1:", additional_in_list1)
+
+# Common vales in lists
+common_vales = set(list1).intersection(set(list2))
+print("Common values in both list:",common_vales)
--- 04_data_structures/4_Sets/set_demo.py
+++ 04_data_structures/4_Sets/set_demo.py
@@ -9,26 +9,28 @@ len(), clear(), copy()
 
 # Creating a set
 my_set = {1, 2, 3, "apple", "banana", 3}
-print("Original set:", my_set)
+print("\nOriginal set:", my_set)
 
 # Adding elements
 my_set.add("orange")
 my_set.update([5, 6, 7])
-print("Set after adding:", my_set)
+print("\nSet after adding:", my_set)
 
 # Removing elements
 my_set.remove(2)
 my_set.discard("banana")
 removed_element = my_set.pop()
-print(f"Removed element: {removed_element}")
-print("Set after pop:", my_set)
+print(f"\nRemoved element: {removed_element}")
+print("\nSet after pop:", my_set)
 
 # Checking membership
-print("Is 'apple' in my_set?", "apple" in my_set)
-print("Length of the set:", len(my_set))
+print("\nIs 'apple' in my_set?", "apple" in my_set)
+print("\nLength of the set:", len(my_set))
 
 # Set Operations
-set2 = {8, 9, 10, 1}
+set2 = {7,8, 9, 10, 1}
+print("\nog set",my_set)
+print("set2",set2)
 print("Union:", my_set.union(set2))
 print("Intersection:", my_set.intersection(set2))
 print("Difference:", my_set.difference(set2))
@@ -36,8 +38,8 @@ print("Symmetric Difference:", my_set.symmetric_difference(set2))
 
 # Iteration
 for item in my_set:
-    print(item)
+    print("\n",item)
 
 # Clearing the set
 my_set.clear()
-print("Set after clear:", my_set)
+print("\nSet after clear:", my_set)
