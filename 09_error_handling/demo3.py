6e8632080392b4bc1a161eed54aabd3ce78c87e7
chayan0104 <chayansamanta8@gmail.com>
2025-02-24 22:35:17 +0530

    new

--- 09_error_handling/demo3.py
+++ 09_error_handling/demo3.py
@@ -1,14 +1,19 @@
 input_no_1 = input("Please enter the first number: ")
 input_no_2 = input("Please enter the second number: ")
 
-# input ---- xyz , 123 , 0 will cause error 
+# input ---- xyz and  0 will cause error 
 try:
+    input_no_1 = float(input_no_1)
+    input_no_2 = float(input_no_2)
+
     division = input_no_1 / input_no_2
     remainder = input_no_1 % input_no_2
-    print(f"Division result is {division} and remainder is {remainder}")
-
+    
 except (ZeroDivisionError,ValueError) as e:
     print(f"Error:{e}")
+
+else:
+    print(f"Division result is {division} and remainder is {remainder}")
 
 finally:
     print("Operation finished")
